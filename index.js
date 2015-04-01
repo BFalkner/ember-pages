e8e7bef9203e87eae585aab33e2c7b64e1aefd1c
Brennan Falkner <Brennan@bfalkner.com>
2015-04-01 05:22:48 -0400

    Process markdown

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel'),
+    MarkdownFilter = require('./lib/filters/markdown');
+
 module.exports = {
-  name: 'ember-pages'
+  name: 'ember-pages',
+
+  treeForPublic: function() {
+    return new MarkdownFilter(new Funnel(this.app.trees.app, {
+      srcDir: '/pages',
+      include: ['**/*.md'],
+      destDir: '/pages',
+      allowEmpty: true
+    }));
+  }
 };
