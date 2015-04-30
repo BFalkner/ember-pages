ea39f3d6e8c52c071fb9bada2102dcce2a561c2f
Brennan Falkner <Brennan@bfalkner.com>
2015-04-30 13:43:56 -0400

    Add page blueprint

---
+++ blueprints/page/index.js
@@ -0,0 +1,36 @@
+var stringUtils = require('ember-cli/lib/utilities/string'),
+    SilentError = require('ember-cli/lib/errors/silent'),
+    EOL = require('os').EOL,
+    _ = require('lodash');
+
+module.exports = {
+  description: 'Creates a static page.',
+
+  locals: function(options) {
+    if (options.entity.options.title === undefined)
+      throw new SilentError("Title is required. ex: `ember generate page "
+                            + options.entity.name + ' title:"My Title"`');
+
+    return {
+      attrs: _(options.entity.options)
+        .pairs()
+        .map(function(pair) {
+          return stringUtils.dasherize(pair[0]) + ": " + pair[1];
+        })
+        .join(EOL),
+      title: options.entity.options.title
+    };
+  },
+
+  fileMapTokens: function() {
+    return {
+      __type__: function(options) {
+        return options.dasherizedModuleName;
+      },
+      __name__: function(options) {
+        var title = options.locals.title;
+        return stringUtils.dasherize(title);
+      }
+    };
+  }
+}
